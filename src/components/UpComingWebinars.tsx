cdb1d25c0376d82cb218cfe2bd3790ac97f2f4f8
zainriaz123 <zainbhaisss356@gmail.com>
2024-11-30 21:06:24 +0500

    ijjefwioj

--- src/components/UpComingWebinars.tsx
+++ src/components/UpComingWebinars.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link"
-import { HoverEffect } from "./ui/card-hover-effect";
+import { HoverEffect } from "./ui/card-hover-effect"
 
 const webinars =[
   {
--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useState } from "react";
 
 export const HoverEffect = ({
- 
   items,
   className,
 }: {
